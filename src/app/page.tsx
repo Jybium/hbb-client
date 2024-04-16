48ba4be6162733ae3d054133b061a3ce067da246
toffee23 <460.bakare@gmail.com>
2024-04-16 21:06:43 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import SplashPage from "@/components/SplashPage";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+      <SplashPage/>
+    </main>
+  );
+}
