bd0fc3c675533de0bf2a88ebdc5253043a6ca49e
jybium <asaolujtomi@gmail.com>
2024-04-29 17:36:39 +0100

    Fixed layout issues

--- src/app/(dashboard)/dashboard/model/layout.tsx
+++ src/app/(dashboard)/dashboard/model/layout.tsx
@@ -9,7 +9,9 @@ import Layout from '@/src/components/app-reusables/explorer/Layout/Layout';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <Layout children={children}/>
+        <Layout>
+            {children}
+        </Layout>
     );
 };
 
